c648a64e18d37de3b36bff7652cf784d177c5ec5
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-11-07 15:53:30 +0530

    Add files via upload

---
+++ Malti dimensional array Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malti_dimensional_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // multi dimensional array 
+
+            int[,] numbers = new int[4, 4];
+
+            for (int i = 0; i < 4; i++) 
+            { 
+                for(int j = 0; j < 4; j++)
+                {
+                    // Console.Write($"* ");
+                    if (i == j)
+                    {
+                        Console.Write("*\t");
+                    }
+                    else
+                    {
+                        Console.Write("\t");
+
+                    }
+                }
+                        
+                Console.WriteLine();
+            }
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
